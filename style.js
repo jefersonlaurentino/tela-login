199085446b38da2b0bebdb9710140acea081828a
jefersonlaurentino <128257769+jefersonlaurentino@users.noreply.github.com>
2023-05-11 18:44:54 -0300

    Initial commit

---
+++ style.js
@@ -0,0 +1,10 @@
+function cliquei() {
+    ver1.style.display = 'none'
+    ver2.style.display = 'block'
+    ver2.style.color = 'orange'
+}
+
+function cliquei2() {
+    ver2.style.display = 'none'
+    ver1.style.display = 'block'
+}
