0f0733824ca4cc880734a580b77fed622c97a3c2
jefersonlaurentino <128257769+jefersonlaurentino@users.noreply.github.com>
2023-05-25 16:46:29 -0300

    melhoria no js

--- style.js
+++ style.js
@@ -1,10 +1,9 @@
 function cliquei() {
-    ver1.style.display = 'none'
-    ver2.style.display = 'block'
-    ver2.style.color = 'orange'
-}
-
-function cliquei2() {
-    ver2.style.display = 'none'
-    ver1.style.display = 'block'
+    if (isenha.type === 'password') {
+        isenha.setAttribute('type', 'text')
+        ver1.innerHTML = 'visibility_off'
+    } else {
+        isenha.setAttribute('type' , 'password')
+        ver1.innerHTML = 'visibility'
+    }
 }
